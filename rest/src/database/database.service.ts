578f57e894817a827bc3537f56668bda57331f9e
SugitaMotoki <ce235019@meiji.ac.jp>
2023-12-16 16:27:32 +0900

    データベースのモジュール・サービスを追加

---
+++ rest/src/database/database.service.ts
@@ -0,0 +1,19 @@
+import type { DataSource } from "typeorm";
+
+export class DatabaseService {
+  public readonly dataSource: DataSource;
+
+  public constructor(dataSource: DataSource) {
+    this.dataSource = dataSource;
+  }
+
+  public async init() {
+    try {
+      await this.dataSource.initialize();
+      console.log("Successfully connected to database.");
+    } catch (error) {
+      console.error("Failed to connect to database.");
+      console.error(error);
+    }
+  }
+}
