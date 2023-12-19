8725d006fe22ca2462ace8aebd3fa1f104e3fd22
SugitaMotoki <ce235019@meiji.ac.jp>
2023-12-20 02:43:47 +0900

    Vuetifyの導入

--- rest-graphql/src/public/main.ts
+++ rest-graphql/src/public/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from "vue";
 import { createRouter, createWebHashHistory } from "vue-router";
+import { createVuetify } from "vuetify";
+import "vuetify/styles";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
 import App from "./App.vue";
 import Home from "./pages/Home.vue";
 import Rest from "./pages/Rest.vue";
@@ -16,6 +21,12 @@ const router = createRouter({
   routes,
 });
 
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
 const app = createApp(App);
 app.use(router);
+app.use(vuetify);
 app.mount("#app");
