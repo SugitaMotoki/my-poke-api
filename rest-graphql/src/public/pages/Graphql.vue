394504e2b9f7b251fd513a64bf25acdcf4bef198
SugitaMotoki <ce235019@meiji.ac.jp>
2023-12-19 20:42:37 +0900

    仮のページを追加

---
+++ rest-graphql/src/public/pages/Graphql.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>GraphQL</h2>
+</template>
---
+++ rest-graphql/src/public/pages/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2>Home</h2>
+  <Hello />
+  <Init />
+  <a href="/graphql">GraphQL</a>
+</template>
+
+<script setup lang="ts">
+import Hello from "../components/Hello.vue";
+import Init from "../components/Init.vue";
+</script>
