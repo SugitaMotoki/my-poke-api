13064b61b4301e7c5ef24858d5c9448ea6cecf64
SugitaMotoki <ce235019@meiji.ac.jp>
2024-01-13 17:42:41 +0900

    サンプルを追加

---
+++ rest-graphql/src/public/utils/samples/pokemon.ts
@@ -0,0 +1,169 @@
+export const pokemon = [
+  {
+    name: "ピカチュウ",
+    pokedex: 25,
+    imageUrl:
+      "https://zukan.pokemon.co.jp/zukan-api/up/images/index/5bb0cfd44302cd4df0c0c88d37457931.png",
+    genus: "ねずみポケモン",
+    height: 0.4,
+    weight: 6.0,
+    generation: 1,
+    typeIds: [8],
+    abilities: [
+      {
+        id: 1,
+        isHidden: false,
+      },
+      {
+        id: 2,
+        isHidden: true,
+      },
+    ],
+  },
+  {
+    name: "ライチュウ",
+    pokedex: 26,
+    imageUrl:
+      "https://zukan.pokemon.co.jp/zukan-api/up/images/index/b044779bd695701e03dd7b3bc83421e9.png",
+    genus: "ねずみポケモン",
+    height: 0.8,
+    weight: 30.0,
+    generation: 1,
+    typeIds: [8],
+    abilities: [
+      {
+        id: 1,
+        isHidden: false,
+      },
+      {
+        id: 2,
+        isHidden: true,
+      },
+    ],
+  },
+  {
+    name: "ウソッキー",
+    pokedex: 185,
+    imageUrl:
+      "https://zukan.pokemon.co.jp/zukan-api/up/images/index/2aef8cf16d4c7ff4e1a0681b1cd3b92e.png",
+    genus: "まねポケモン",
+    height: 1.2,
+    weight: 38.0,
+    generation: 2,
+    typeIds: [9],
+    abilities: [
+      {
+        id: 6,
+        isHidden: false,
+      },
+      {
+        id: 7,
+        isHidden: false,
+      },
+      {
+        id: 8,
+        isHidden: true,
+      },
+    ],
+  },
+  {
+    name: "ウソハチ",
+    pokedex: 438,
+    imageUrl:
+      "https://zukan.pokemon.co.jp/zukan-api/up/images/index/6a62db091fe28711be5dc4e5fdc44688.png",
+    genus: "ぼんさいポケモン",
+    height: 0.5,
+    weight: 15.0,
+    generation: 2,
+    typeIds: [9],
+    abilities: [
+      {
+        id: 6,
+        isHidden: false,
+      },
+      {
+        id: 7,
+        isHidden: false,
+      },
+      {
+        id: 8,
+        isHidden: true,
+      },
+    ],
+  },
+  {
+    name: "ヒトモシ",
+    pokedex: 607,
+    imageUrl:
+      "https://zukan.pokemon.co.jp/zukan-api/up/images/index/092cb9932eeb69ce09a0c95d979bfc54.png",
+    genus: "ろうそくポケモン",
+    height: 0.3,
+    weight: 3.1,
+    generation: 5,
+    typeIds: [2, 14],
+    abilities: [
+      {
+        id: 3,
+        isHidden: false,
+      },
+      {
+        id: 4,
+        isHidden: false,
+      },
+      {
+        id: 5,
+        isHidden: true,
+      },
+    ],
+  },
+  {
+    name: "ランプラー",
+    pokedex: 608,
+    imageUrl:
+      "https://zukan.pokemon.co.jp/zukan-api/up/images/index/13159ceffda14062f986e9794a9c1b83.png",
+    genus: "ランプポケモン",
+    height: 0.6,
+    weight: 13.0,
+    generation: 5,
+    typeIds: [2, 14],
+    abilities: [
+      {
+        id: 3,
+        isHidden: false,
+      },
+      {
+        id: 4,
+        isHidden: false,
+      },
+      {
+        id: 5,
+        isHidden: true,
+      },
+    ],
+  },
+  {
+    name: "シャンデラ",
+    pokedex: 609,
+    imageUrl:
+      "https://zukan.pokemon.co.jp/zukan-api/up/images/index/8519cf8728863fcf86fba321c2075bf2.png",
+    genus: "いざないポケモン",
+    height: 1.0,
+    weight: 34.3,
+    generation: 5,
+    typeIds: [2, 14],
+    abilities: [
+      {
+        id: 3,
+        isHidden: false,
+      },
+      {
+        id: 4,
+        isHidden: false,
+      },
+      {
+        id: 5,
+        isHidden: true,
+      },
+    ],
+  },
+];
